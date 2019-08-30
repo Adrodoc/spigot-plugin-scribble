ef87ece3802b39067606eecc54154d7b4578991d
Adrodoc <adrodoc55@googlemail.com>
2019-08-30 22:50:04 +0200

    initial commit

---
+++ src/main/java/de/adrodoc/scribble/ScribblePlugin.java
@@ -0,0 +1,10 @@
+package de.adrodoc.scribble;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ScribblePlugin extends JavaPlugin {
+  @Override
+  public void onEnable() {
+    System.out.println("Hello");
+  }
+}
